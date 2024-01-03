bace5c9958d05af58f69be13f2265134b0040897
Bhavya Anand <bhavyaanand536@gmail.com>
2024-01-03 13:11:51 +0530

    Created a parallax background

---
+++ Components/Backgrounds/Parallax-Background/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+
+    text.style.marginTop = value * 1 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 0.8 + 'px';
+
+});
