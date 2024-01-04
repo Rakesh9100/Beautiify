21eee4b6d9def621ef345d01fdd1600b014fe5bd
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-01-04 11:40:33 +0530

    Scroll To Top Btn 🔝

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+let calcScrollValue = () => {
+    let scrollProg = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProg.style.display = "grid";
+    } else {
+        scrollProg.style.display = "none";
+    }
+    scrollProg.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProg.style.background = `conic-gradient(#0063ba ${scrollValue}%, #d499de ${scrollValue}%)`;
+};
+
+window.addEventListener('scroll', function () {
+    var scrollToTopButton = document.getElementById('progress');
+    if (window.pageYOffset > 200) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
