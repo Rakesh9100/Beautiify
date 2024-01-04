074536f30cedc06b4070e57190371f8898d49b8f
Saurav Kumar <125905089+saurav2208@users.noreply.github.com>
2024-01-04 06:30:49 +0000

    Added an accordion

---
+++ Components/Accordions/Modern-Accordion/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("header");
+    header.addEventListener("click", () => {
+        item.classList.toggle("open");
+
+        let description = item.querySelector(".description");
+        if (item.classList.contains("open")) {
+            description.style.height = `${description.scrollHeight}px`; //scrollHeight property returns the height of an element including padding , but excluding borders, scrollbar or margin
+            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        } else {
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+        removeOpen(index); //calling the funtion and also passing the index number of the clicked header
+    })
+})
+
+function removeOpen(index1) {
+    accordionContent.forEach((item2, index2) => {
+        if (index1 != index2) {
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".description");
+            des.style.height = "0px";
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    })
+}
