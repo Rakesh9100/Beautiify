c37223d9545a593ba6813230d39f28869f5765f5
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-01-05 23:02:31 +0530

    Contributors Enlisted (#48)

---
+++ assets/js/contributor.js
@@ -0,0 +1,53 @@
+const cont = document.getElementById('contributor');
+const owner = 'Rakesh9100';
+const repoName = 'Beautiify';
+
+async function fetchContributors(pageNumber) {
+    const perPage = 100;
+    const url = `https://api.github.com/repos/${owner}/${repoName}/contributors?page=${pageNumber}&per_page=${perPage}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch contributors data. Status code: ${response.status}`);
+    }
+
+    const contributorsData = await response.json();
+    return contributorsData;
+}
+
+// Function to fetch all contributors
+async function fetchAllContributors() {
+    let allContributors = [];
+    let pageNumber = 1;
+
+    try {
+        while (true) {
+            const contributorsData = await fetchContributors(pageNumber);
+            if (contributorsData.length === 0) {
+                break;
+            }
+            allContributors = allContributors.concat(contributorsData);
+            pageNumber++;
+        }
+        allContributors.forEach((contributor) => {
+            const contributorCard = document.createElement('div');
+            contributorCard.classList.add('contributor-card');
+
+            const avatarImg = document.createElement('img');
+            avatarImg.src = contributor.avatar_url;
+            avatarImg.alt = `${contributor.login}'s Picture`;
+
+            const loginLink = document.createElement('a');
+            loginLink.href = contributor.html_url;
+            loginLink.appendChild(avatarImg);
+
+            contributorCard.appendChild(loginLink);
+
+            cont.appendChild(contributorCard);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchAllContributors();
