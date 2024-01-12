61f9ecdcce019f70816deb2b351311decd0518f9
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-01-12 15:41:33 +0530

    Added Scroll Reveal effect (#158)

---
+++ scroll-reveal.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+	reset: false,
+	distance: "40px",
+	duration: 2500,
+	delay: 400,
+});
+
+ScrollReveal().reveal(".navbar,.heading,.box,.paragraph,.started", {
+	delay: 400,
+	origin: "top",
+});
+ScrollReveal().reveal(".started,.logo,#contributor,.sub-head,.footer", {
+	delay: 500,
+	origin: "top",
+});
