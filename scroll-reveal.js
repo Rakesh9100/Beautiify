2e13bde96b3388ca846c857c0b83090577260314
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-01-12 11:37:47 +0530

    Add scroll reveal animation to elements

---
+++ scroll-reveal.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+	reset: false,
+	distance: "40px",
+	duration: 2500,
+	delay: 400,
+});
+
+ScrollReveal().reveal(".navbar,.logo,.heading,.sub-head,.paragraph,.started", {
+	delay: 400,
+	origin: "top",
+});
