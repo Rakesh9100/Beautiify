77ff02b79371d166a98bfc0241e76f66d26e0d3a
Archiesachin <archieshah8767@gmail.com>
2024-06-08 18:38:09 +0530

    draggable carousel added

---
+++ Components/Carousels/Draggable-Carousel/script.js
@@ -0,0 +1,32 @@
+const track = document.getElementById("image-track");
+
+window.onmousedown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
+};
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
