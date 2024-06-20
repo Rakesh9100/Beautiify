3eb055fba879b52c46cca58bd882842c73897071
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-06-20 14:22:37 +0530

    added theme toggle dropdown

---
+++ Components/Dropdowns/Theme-Toggle-Dropdown/script.js
@@ -0,0 +1,22 @@
+const dark = document.querySelector("#dark-theme");
+const light = document.querySelector("#light-theme");
+const defaultT = document.querySelector("#default-theme");
+
+dark.addEventListener("click", () => {
+	document.body.classList.add("dark-theme");
+});
+
+light.addEventListener("click", () => {
+	document.body.classList.remove("dark-theme");
+});
+
+defaultT.addEventListener("click", () => {
+	if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches
+	) {
+		document.body.classList.add("dark-theme");
+	} else {
+		document.body.classList.remove("dark-theme");
+	}
+});
