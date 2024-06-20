d4d430ccf666ac2e0d7e731877a5dd86491e9060
Rajveer <142779809+root-0101@users.noreply.github.com>
2024-06-20 07:39:20 +0000

    Add navigation-bar

---
+++ Components/Navigation-Bars/Collapsible-Navigation-Bar/script.js
@@ -0,0 +1,21 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+function menuBtnChange() {
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else {
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+  }
+}
