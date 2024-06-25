9ee97949cef163c3a421af08cc1e7e683c95c4bc
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-06-25 23:39:12 +0530

    Update script.js

--- Components/Navigation-Bars/Collapsible-Navigation-Bar/script.js
+++ Components/Navigation-Bars/Collapsible-Navigation-Bar/script.js
@@ -3,19 +3,19 @@ let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
+    sidebar.classList.toggle("open");
+    menuBtnChange();
 });
 
 searchBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
+    sidebar.classList.toggle("open");
+    menuBtnChange();
 });
 
 function menuBtnChange() {
-  if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-  } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-  }
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
 }
