ce5763a6d7c978f6e8a079922e2aaf1bf0e0bc02
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-06-26 19:22:04 +0530

    Update script.js

--- Components/Dropdowns/Theme-Toggle-Dropdown/script.js
+++ Components/Dropdowns/Theme-Toggle-Dropdown/script.js
@@ -3,20 +3,20 @@ const light = document.querySelector("#light-theme");
 const defaultT = document.querySelector("#default-theme");
 
 dark.addEventListener("click", () => {
-	document.body.classList.add("dark-theme");
+    document.body.classList.add("dark-theme");
 });
 
 light.addEventListener("click", () => {
-	document.body.classList.remove("dark-theme");
+    document.body.classList.remove("dark-theme");
 });
 
 defaultT.addEventListener("click", () => {
-	if (
-		window.matchMedia &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches
-	) {
-		document.body.classList.add("dark-theme");
-	} else {
-		document.body.classList.remove("dark-theme");
-	}
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
 });
