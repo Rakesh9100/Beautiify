f7d08b2558ea1f29b748d9eccfdbe40063d68ce2
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-07-07 12:43:32 +0530

    Update script.js

--- Components/Cards/Port-ID-Card/script.js
+++ Components/Cards/Port-ID-Card/script.js
@@ -3,18 +3,18 @@ const sections = document.querySelectorAll(".card-section");
 const card = document.querySelector(".card");
 
 const handleButtonClick = e => {
-  const targetSection = e.target.getAttribute("data-section");
-  const section = document.querySelector(targetSection);
-  targetSection !== "#about" ?
-  card.classList.add("is-active") :
-  card.classList.remove("is-active");
-  card.setAttribute("data-state", targetSection);
-  sections.forEach(s => s.classList.remove("is-active"));
-  buttons.forEach(b => b.classList.remove("is-active"));
-  e.target.classList.add("is-active");
-  section.classList.add("is-active");
+    const targetSection = e.target.getAttribute("data-section");
+    const section = document.querySelector(targetSection);
+    targetSection !== "#about" ?
+        card.classList.add("is-active") :
+        card.classList.remove("is-active");
+    card.setAttribute("data-state", targetSection);
+    sections.forEach(s => s.classList.remove("is-active"));
+    buttons.forEach(b => b.classList.remove("is-active"));
+    e.target.classList.add("is-active");
+    section.classList.add("is-active");
 };
 
 buttons.forEach(btn => {
-  btn.addEventListener("click", handleButtonClick);
+    btn.addEventListener("click", handleButtonClick);
 });
