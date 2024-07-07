95b0456637bda3d89eacbdbc76f93e92d74b3825
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-07-07 13:22:32 +0530

    Update script.js

--- Components/Footers/Interactive-Footer/script.js
+++ Components/Footers/Interactive-Footer/script.js
@@ -1,22 +1,20 @@
-
-
 const moreInfoButton = document.querySelector('.more-info-button');
 const moreInfoSection = document.querySelector('.more-info');
 
 moreInfoButton.addEventListener('click', () => {
-  moreInfoSection.classList.toggle('show');
-  if (moreInfoSection.classList.contains('show')) {
-    moreInfoButton.textContent = 'Less Info';
-  } else {
-    moreInfoButton.textContent = 'More Info';
-  }
+    moreInfoSection.classList.toggle('show');
+    if (moreInfoSection.classList.contains('show')) {
+        moreInfoButton.textContent = 'Less Info';
+    } else {
+        moreInfoButton.textContent = 'More Info';
+    }
 });
 
 const scrollToTopButton = document.querySelector('.scroll-to-top');
 
 scrollToTopButton.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth' 
-  });
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
