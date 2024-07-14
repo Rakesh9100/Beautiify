0c539256dfd7c7e028ee4d1c44c4ab5e6e52fc44
Rajveer <142779809+root-0101@users.noreply.github.com>
2024-07-14 16:14:13 +0000

    add subscribe popup

---
+++ Components/Popups/Subscribe-Popup/script.js
@@ -0,0 +1,10 @@
+
+var popup = document.getElementById('popup');
+document.getElementById('btn').addEventListener('click', function show() {
+    popup.style.display = "block";
+}
+    );
+document.getElementById('close').addEventListener('click', function hide(){
+    popup.style.display = "none";
+}
+    );
