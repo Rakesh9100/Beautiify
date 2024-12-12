bdbfbfdcafb785c1d8ca2d825b715940bbd56c1b
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-12-12 09:37:06 +0530

    Added changes

--- Components/Forms/Swag-Shipment-Form/script.js
+++ Components/Forms/Swag-Shipment-Form/script.js
@@ -1,33 +1,11 @@
-const inputs = document.querySelectorAll('input');
-const submitBtn = document.getElementById('submit-btn');
+const form = document.getElementById('shipment-form');
 const submissionMessage = document.getElementById('submission-message');
 
-inputs.forEach(input => {
-    input.addEventListener('input', () => {
-        let allFilled = true;
-
-        for (let input of inputs) {
-            if (input.value.trim() === '') {
-                allFilled = false;
-                break;
-            }
-        }
-
-        if (allFilled) {
-            submitBtn.classList.add('active');
-        } else {
-            submitBtn.classList.remove('active');
-        }
-    });
-});
-
-submitBtn.addEventListener('click', () => {
-    if (submitBtn.classList.contains('active')) {
-        inputs.forEach(input => (input.value = ''));
-        submissionMessage.style.display = 'block';
-        setTimeout(() => {
-            submissionMessage.style.display = 'none';
-        }, 3000);
-        submitBtn.classList.remove('active');
-    }
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.reset();
+    submissionMessage.style.display = 'block';
+    setTimeout(() => {
+        submissionMessage.style.display = 'none';
+    }, 3000);
 });
