639db8bf4a311f3c31969fc4cae57e61462668d4
Jitendra Suthar <79091909+jitendrasuthar1998@users.noreply.github.com>
2024-12-31 21:08:32 +0530

    Made Animated Login Register Form component responsive (#1434)

--- Components/Forms/Animated-Login-Register-Form/script.js
+++ Components/Forms/Animated-Login-Register-Form/script.js
@@ -13,3 +13,24 @@ loginButton.addEventListener("click", () => {
 function refreshPage() {
     location.reload();
 }
+
+const smallScreenRegisterButton = document.getElementById("go-to-register-btn");
+const smallScreenRegisterContainer = document.getElementById("small-screen-register-container");
+const smallScreenLoginContainer = document.getElementById("small-screen-login-container");
+const smallScreenLoginButton = document.getElementById("go-to-login-btn");
+
+smallScreenRegisterButton.addEventListener("click", showRegister)
+
+smallScreenLoginButton.addEventListener("click", showLogin);
+
+// Function to show the login form
+function showLogin() {
+    document.getElementById('register-form').style.display = 'none'; // Hide register form
+    document.getElementById('login-form').style.display = 'block'; // Show login form
+}
+
+// Function to show the register form
+function showRegister() {
+    document.getElementById('login-form').style.display = 'none'; // Hide login form
+    document.getElementById('register-form').style.display = 'block'; // Show register form
+}
