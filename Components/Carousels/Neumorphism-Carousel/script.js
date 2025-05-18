44298f6e66b23179a77730c50bb556f7a45708a4
Jouwy <132906518+Jouwy@users.noreply.github.com>
2025-05-18 21:21:04 +0800

    Made Neumorphism Carousel component responsive (2)

--- Components/Carousels/Neumorphism-Carousel/script.js
+++ Components/Carousels/Neumorphism-Carousel/script.js
@@ -5,9 +5,25 @@ var swiper = new Swiper(".mySwiper", {
     autoplay: {
         delay: 4000,
         disableOnInteraction: false,
+        pauseOnMouseEnter: true,
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    observer: true,
+    observeParents: true,
+    resizeObserver: true,
+});
+
+// Debounce resize updates to prevent shaking
+let resizeTimeout;
+window.addEventListener('resize', () => {
+    swiper.autoplay.stop();
+
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        swiper.update();
+        swiper.autoplay.start();
+    }, 150);
 });
